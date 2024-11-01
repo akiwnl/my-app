d462eaaa177f5d97f5293a91f535dffb9807c936
akiwnl <carvalhokaike69@gmail.com>
2024-11-01 12:43:11 -0300

    add Lista de usuarios and Logout

--- src/app/portal/page.tsx
+++ src/app/portal/page.tsx
@@ -1,7 +1,13 @@
+import Link from "next/link";
+import UsersList from "../../modules/users/components/users-list";
+
 export default function PortalPage() {
   return (
     <main className="flex min-h-screen flex-col items-center p-24">
-      <h1 className="text-4xl font-bold">Portal</h1>
+      <h1 className="text-4xl font-bold">Lista de usuários</h1>
+      <UsersList />
+
+      <Link href="/api/logout">Logout</Link>
     </main>
   );
 }
